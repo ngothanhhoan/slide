1e7f4ace81ec15cf0257731879e49d8e97be073b
ngothanhhoan <ngothanhhoan2108@gmail.com>
2022-09-29 18:09:28 +0700

    upload sound code

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ setInterval(function () {
     counter = 1;
   }
 }, 5000);
+
+function myFunction() {
+  var x = document.getElementById("ads-background");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("button-open").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("button-open").style.display = "block";
+}
